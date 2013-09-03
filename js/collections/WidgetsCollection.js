983c02e65b8eb1c63f7d01dda8b7342531c0a70d
Antonio Marino <antomarino93@gmail.com>
2013-09-03 02:24:57 +0200

    removed some temporary code

--- js/collections/WidgetsCollection.js
+++ js/collections/WidgetsCollection.js
@@ -45,48 +45,7 @@ define([ "jquery", "backbone", "models/WidgetModel" ],
 
                 return 'ObjectMeta' + ':' + this.parent.get('className')
             },
-
-/*          Temporary: use this method only if staging or dev servers are not available
-
-            // Overriding Backbone.fetch method
-            fetch: function(options) {
-                
-                // As in the backbone source
-                options = options ? _.clone(options) : {};
-                if (options.parse === void 0) options.parse = true;
-                var success = options.success;
-                var collection = this;
-                options.success = function(resp) {
-                var method = options.reset ? 'reset' : 'set';
-                collection[method](resp, options);
-                if (success) success(collection, resp, options);
-                    collection.trigger('sync', collection, resp, options);
-                };
-                //return this.sync('read', this, options);
-                
-                // MY CODE
-                // Define my own URL (fetching from 'species')
-                options.url = window.BASE_URL + "/rest/config/sections/species/" 
-                            + options.className + "?content-type=application/json";
-
-                options.async = false;
-                
-                // do the request
-                var xhr = this.sync('read', this, options);
-
-                // if the previous request haven't found the class
-                if (xhr.responseJSON.data == null) {
-
-                    // redefine the url (now fetch from 'resources')
-                    options.url = options.url.replace("species", "resources");
-
-                    // re-do the request
-                    xhr = this.sync('read', this, options)
-                }
-
-                return xhr;
-            },
-*/
+            
         } );
 
         return WidgetsCollection;
