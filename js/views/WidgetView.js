19fe28d8736b96926c6b412e2973f522e0a0059e
Antonio Marino <antomarino93@gmail.com>
2013-09-19 21:52:10 +0100

    moved tooltips code from WidgetView to utils

--- js/views/WidgetView.js
+++ js/views/WidgetView.js
@@ -19,25 +19,6 @@ define([ "jquery",
                 this.model.view = this;
 
                 this.model.on("change:visible", this.toggleVisibility, this);
-
-                this.spinnerOpts = {
-                    lines: 13, // The number of lines to draw
-                    length: 20, // The length of each line
-                    width: 10, // The line thickness
-                    radius: 30, // The radius of the inner circle
-                    corners: 1, // Corner roundness (0..1)
-                    rotate: 0, // The rotation offset
-                    direction: 1, // 1: clockwise, -1: counterclockwise
-                    color: '#000', // #rgb or #rrggbb or array of colors
-                    speed: 1, // Rounds per second
-                    trail: 60, // Afterglow percentage
-                    shadow: false, // Whether to render a shadow
-                    hwaccel: false, // Whether to use hardware acceleration
-                    className: 'spinner', // The CSS class to assign to the spinner
-                    zIndex: 2e9, // The z-index (defaults to 2000000000)
-                    top: 'auto', // Top position relative to parent in px
-                    left: 'auto' // Left position relative to parent in px
-                };
             },
 
             // Renders the placeholder for this widget
@@ -61,76 +42,9 @@ define([ "jquery",
                     $("#" + this.id + "Popup").popup('open');
                 } );
 
-                this.parent.parent.$el.off('mouseenter', "[rel~=tooltip]")
-                                      .on('mouseenter', '[rel~=tooltip]', function() {
-
-                    target  = $( this );
-                    tip     = target.attr( 'title' );
-                    tooltip = $( '<div id="tooltip"></div>' );
-             
-                    if( !tip || tip == '' )
-                        return false;
-
-                    target.removeAttr( 'title' );
-                    tooltip.css( 'opacity', 0 )
-                           .html( tip )
-                           .appendTo( 'body' );
-
-                    var init_tooltip = function()
-                    {
-                        if( $( window ).width() < tooltip.outerWidth() * 1.5 )
-                            tooltip.css( 'max-width', $( window ).width() / 2 );
-                        else
-                            tooltip.css( 'max-width', 340 );
-             
-                        var pos_left = target.offset().left + ( target.outerWidth() / 2 ) - ( tooltip.outerWidth() / 2 ),
-                            pos_top  = target.offset().top - tooltip.outerHeight() - 20;
-             
-                        if( pos_left < 0 )
-                        {
-                            pos_left = target.offset().left + target.outerWidth() / 2 - 20;
-                            tooltip.addClass( 'left' );
-                        }
-                        else
-                            tooltip.removeClass( 'left' );
-             
-                        if( pos_left + tooltip.outerWidth() > $( window ).width() )
-                        {
-                            pos_left = target.offset().left - tooltip.outerWidth() + target.outerWidth() / 2 + 20;
-                            tooltip.addClass( 'right' );
-                        }
-                        else
-                            tooltip.removeClass( 'right' );
-             
-                        if( pos_top < 0 )
-                        {
-                            var pos_top  = target.offset().top + target.outerHeight();
-                            tooltip.addClass( 'top' );
-                        }
-                        else
-                            tooltip.removeClass( 'top' );
-             
-                        tooltip.css( { left: pos_left, top: pos_top } )
-                               .animate( { top: '+=10', opacity: 1 }, 50 );
-                    };
-             
-                    init_tooltip();
-                    $( window ).resize( init_tooltip );
-             
-                    var remove_tooltip = function()
-                    {
-                        tooltip.animate( { top: '-=10', opacity: 0 }, 50, function()
-                        {
-                            $( this ).remove();
-                        });
-             
-                        target.attr( 'title', tip );
-                    };
-             
-                    target.on( 'mouseleave', remove_tooltip );
-                    tooltip.on( 'click', remove_tooltip );
-
-                } );
+                WBMobile.views.object.$el.off('mouseenter', "[rel~=tooltip]")
+                                      .on('mouseenter', '[rel~=tooltip]', 
+                                        WBMobile.utils.tooltipCallback);
 
                 // Maintains chainability
                 return this;
@@ -482,7 +396,6 @@ define([ "jquery",
                     }
                     else {
                         // external
-
                         // modify "a" tag to indicate that it's an external resource
                         this.target = "_system";
                     }
